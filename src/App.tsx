0ce89433fad25ac33cd2e8f728e46d00de84196d
TheLogan <timlogan475@gmail.com>
2021-10-01 00:44:24 +0200

    Re ordered some components in routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,30 @@ import CardBrowser from './Pages/CardBrowser';
 import Game from './Pages/Game';
 import MainMenu from './Pages/MainMenu';
 import 'typeface-roboto'
+import GameMenu from './Pages/GameMenu';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/browse">
+          <Route path="/admin/browse">
             <Layout>
               <CardBrowser />
             </Layout>
           </Route>
-          <Route path="/play">
+
+          <Route path="/game">
+            <Layout>
+              <GameMenu />
+            </Layout>
+          </Route>
+          <Route path="/game/play">
             <Layout>
               <Game />
             </Layout>
           </Route>
+
           <Route path="/">
             <Layout>
               <MainMenu />
--- src/Pages/Game/index.tsx
+++ src/Pages/Game/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 const Game = () => {
-  return <></>;
+  return <>This is game</>;
 }
 
 export default Game;
