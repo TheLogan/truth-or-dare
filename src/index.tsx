79f29fadf7f263528be00d0cf2dacc9ef4c31793
TheLogan <timlogan475@gmail.com>
2021-10-03 14:49:45 +0200

    Added debug message

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log('starting react')
 ReactDOM.render(
   <React.StrictMode>
     <App />
