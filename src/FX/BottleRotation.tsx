4100dbbb0eab46333fe49fde93dde53e319f7ec8
Logan <timlogan475@gmail.com>
2021-10-16 21:42:11 +0200

    Oddly fixed bottle rotation o_0

--- src/FX/BottleRotation.tsx
+++ src/FX/BottleRotation.tsx
@@ -2,31 +2,24 @@ import { CSSProperties, useEffect } from "react";
 import { iBottleRotation } from "./interfaces";
 
 const BottleRotation: React.FC<iBottleRotation> = (props) => {
-  
   useEffect(() => {
-    if(props.rotate === true) {
+    if (props.rotate === true) {
       setTimeout(() => {
         props.onDone();
-      }, props.timing);
+      }, props.timing + 1000);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.rotate])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.rotate]);
 
-  
   const style: CSSProperties = {
-    display: 'inline-block',
-    backfaceVisibility: 'hidden',
-    transform: props.rotate ?
-      `rotate(${props.rotation}deg)` :
-      `rotate(0deg)`,
+    display: "inline-block",
+    backfaceVisibility: "hidden",
+    transform: props.rotate ? `rotate(${props.rotation}deg)` : `rotate(0deg)`,
     transition: `transform ${props.timing}ms`,
-    width: '100%'
-  }
+    width: "100%",
+  };
 
-  return <span style={style}>
-        {props.children}
-  </span>
-}
-
+  return <span style={style}>{props.children}</span>;
+};
 
 export default BottleRotation;
