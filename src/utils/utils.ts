cd856686aa81a9a3975293100559e7ba6ca951b3
Logan <timlogan475@gmail.com>
2021-10-24 01:11:59 +0200

    Attempts to fix replaceAll issues with cordova

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,30 @@
 import eCard from "../Entities/eCard";
-import { brandCategory, drinkOrStrip, getAge, getDirection, getLeastMost, getTitle, oneOrTwoPieces, wordEnding } from "./randomizers";
+import {
+  brandCategory,
+  drinkOrStrip,
+  getAge,
+  getDirection,
+  getLeastMost,
+  getTitle,
+  oneOrTwoPieces,
+  wordEnding,
+} from "./randomizers";
+import 'react-app-polyfill/stable'
 
 export const capitalizeFirstLetter = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
-}
+};
 
-export const randomRange = (min: number = 0, max: number = 10) => Math.floor(Math.random() * (max - min + 1) + min)
+export const randomRange = (min: number = 0, max: number = 10) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
 
 export const shuffleCards = (cards: eCard[]) => {
   const cardArr: eCard[] = [];
-  cards.forEach(card => { for (let i = 0; i < card.cardCount; i++) { cardArr.push(card); } });
+  cards.forEach((card) => {
+    for (let i = 0; i < card.cardCount; i++) {
+      cardArr.push(card);
+    }
+  });
 
   const shuffledDeck: eCard[] = [];
   while (cardArr.length > 0) {
@@ -17,29 +32,32 @@ export const shuffleCards = (cards: eCard[]) => {
     shuffledDeck.push(...cardArr.splice(index, 1));
   }
   return shuffledDeck;
-}
-
+};
 
 export const getRotation = () => {
   let rot = randomRange(0, 360);
-  return rot + (360 * randomRange(3, 6));
-}
+  return rot + 360 * randomRange(3, 6);
+};
 
 export const formatCardText = (description: string) => {
   const rangeReg = /\[\d{1,2}..\d{1,2}\]/g;
-  
+
   let descr = description;
   let ranges = rangeReg.exec(descr);
   if (ranges) {
     for (const ra of ranges) {
-      let str = ra.replace('[', '');
-      str = str.replace(']', '');
-      const [min, max] = str.split('..');
-      descr = descr.replace(ra, Math.round(randomRange(Number(min), Number(max))).toString())
+      let str = ra.replace("[", "");
+      str = str.replace("]", "");
+      const [min, max] = str.split("..");
+      descr = descr.replace(
+        ra,
+        Math.round(randomRange(Number(min), Number(max))).toString()
+      );
     }
   }
-  
-  descr = descr.replaceAll("*12pieces*", oneOrTwoPieces())
+
+
+  descr = descr.replaceAll("*12pieces*", oneOrTwoPieces());
   descr = descr.replaceAll("*direction*", getDirection());
   descr = descr.replaceAll("*wordEnding*", wordEnding());
   descr = descr.replaceAll("*title*", getTitle());
@@ -47,10 +65,10 @@ export const formatCardText = (description: string) => {
   descr = descr.replaceAll("*leastMost*", getLeastMost());
   descr = descr.replaceAll("*drinkStrip*", drinkOrStrip());
   const [brandName, category] = brandCategory();
-  descr = descr.replaceAll('*brandName*', brandName);
-  descr = descr.replaceAll('*category*', category);
+  descr = descr.replaceAll("*brandName*", brandName);
+  descr = descr.replaceAll("*category*", category);
   return descr;
-}
+};
 
 /* IDEAS */
-    // Countdown function
+// Countdown function
